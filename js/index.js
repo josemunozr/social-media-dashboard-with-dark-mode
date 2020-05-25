dd62e9b381355704398bdec7e6107c2299ca6a33
José Muñoz Ruiz <jozeepp@gmail.com>
2020-05-25 09:09:21 -0400

    darkmode active

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const inputSwitch = document.getElementById('inputSwitch')
-
+const dashboard = document.getElementById('dashboard')
 inputSwitch.addEventListener('click', () => {
-  inputSwitch.classList.toggle('on')
+  inputSwitch.classList.toggle('on');
+  dashboard.classList.toggle('darkmode')
 })
