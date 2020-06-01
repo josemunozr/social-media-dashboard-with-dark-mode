1cabf49af8a2654e90c99343f476bf5e00ddb01a
José Muñoz Ruiz <jozeepp@gmail.com>
2020-06-01 08:23:25 -0400

    old version files are deleted

--- js/index.js
+++
@@ -1,6 +0,0 @@
-const inputSwitch = document.getElementById('inputSwitch')
-const dashboard = document.getElementById('dashboard')
-inputSwitch.addEventListener('click', () => {
-  inputSwitch.classList.toggle('on');
-  dashboard.classList.toggle('darkmode')
-})
